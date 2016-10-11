fc7dba867122c7e4421df2782b8fcf361d093f5f
Jan Engelmohr <jan.engelmohr@mailbox.tu-dresden.de>
2016-10-11 13:12:54 +0200

    add mknod - who knows...

--- p8000-vendor-blobs.mk
+++ p8000-vendor-blobs.mk
@@ -522,6 +522,7 @@ PRODUCT_COPY_FILES += \
     vendor/elephone/p8000/proprietary/system/vendor/lib/mediadrm/libwvdrmengine.so:system/vendor/lib/mediadrm/libwvdrmengine.so \
     vendor/elephone/p8000/proprietary/system/vendor/lib64/mediadrm/libmockdrmcryptoplugin.so:system/vendor/lib64/mediadrm/libmockdrmcryptoplugin.so \
     vendor/elephone/p8000/proprietary/system/xbin/mnld:system/xbin/mnld \
+    vendor/elephone/p8000/proprietary/system/xbin/mknod:system/xbin/mknod \
     vendor/elephone/p8000/proprietary/system/vendor/pittpatt/models/recognition/face.face.y0-y0-71-N-tree_7-wmd.bin:system/vendor/pittpatt/models/recognition/face.face.y0-y0-71-N-tree_7-wmd.bin \
     vendor/elephone/p8000/proprietary/system/vendor/pittpatt/models/detection/multi_pose_face_landmark_detectors.8/landmark_group_meta_data.bin:system/vendor/pittpatt/models/detection/multi_pose_face_landmark_detectors.8/landmark_group_meta_data.bin \
     vendor/elephone/p8000/proprietary/system/vendor/pittpatt/models/detection/multi_pose_face_landmark_detectors.8/left_eye-y0-yi45-p0-pi45-r0-ri20.lg_32-tree7-wmd.bin:system/vendor/pittpatt/models/detection/multi_pose_face_landmark_detectors.8/left_eye-y0-yi45-p0-pi45-r0-ri20.lg_32-tree7-wmd.bin \
